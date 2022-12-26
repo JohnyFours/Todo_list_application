981d25b3444478d0ee37b57aeec99fdcb04fc0f3
Oleksandr Protasov <aleks.protasov@outlook.com>
2022-12-26 17:49:34 +0200

    commit Todo List application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css'
+import {Toast} from "bootstrap";
+import AddTask from "./components/AddTask";
+import UpdateTask from "./components/UpdateTask";
+import Todo from "./components/Todo";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todo, setTodo] = useState([]);
+
+    const [newTask, setNewTask] = useState("");
+    const [updateData, setUpdateData] = useState("");
+
+    const addTask = () => {
+        if (newTask) {
+            let num = todo.length + 1
+            let newEntry = {id: num, title: newTask, status: false}
+            setTodo([...todo, newEntry])
+            setNewTask('')
+        }
+    }
+
+    const deleteTask = (id) => {
+        let newTask = todo.filter(task => task.id !== id)
+        setTodo(newTask)
+
+    };
+
+    const markDone = (id) => {
+        let newTask = todo.map(task => {
+            if (task.id === id) {
+                return ({...task, status: !task.status})
+            }
+            return task
+        })
+
+        setTodo(newTask)
+
+    };
+
+    const cancelUpdate = () => {
+        setUpdateData('');
+
+    };
+    const changeTask = (e) => {
+        let newEntry = {
+            id: updateData.id, title: e.target.value, status: updateData.status ? true : false
+        }
+
+        setUpdateData(newEntry)
+
+    };
+
+    const updateTask = () => {
+        let filterRecords = [...todo].filter(task => task.id !== updateData.id);
+        let updatedObject = [...filterRecords, updateData]
+
+        setTodo(updatedObject)
+        setUpdateData('')
+    };
+
+
+    return (<div className=" container App">
+        <header className='header'>
+            <h1>Todo List Project</h1>
+        </header>
+
+        {updateData && updateData ? (
+            <UpdateTask
+                updateData={updateData}
+                changeTask={changeTask}
+                updateTask={updateTask}
+                cancelUpdate={cancelUpdate}/>) : (
+            <AddTask
+                newTask={newTask}
+                setNewTask={setNewTask}
+                addTask={addTask}/>
+        )}
+        <br/>
+
+        {todo && todo.length ? '' : "No Task..."}
+
+        <Todo todo={todo}
+              markDone={markDone}
+              setUpdateData={setUpdateData}
+              deleteTask={deleteTask}/>
+    </div>);
 }
 
 export default App;
---
+++ src/components/UpdateTask.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const UpdateTask = ({updateData,changeTask,updateTask, cancelUpdate}) => {
+    return (
+        <>
+                <div className="row">
+                    <div className="col">
+                        <input className="form-control form-control-lg"
+                               onChange={(e) => changeTask(e)}
+                               value={updateData && updateData.title}/>
+                    </div>
+                    <div className="col-auto">
+                        <button
+                            className="btn btn-lg btn-success mr-20"
+                            onClick={updateTask}>
+                            Update
+                        </button>
+                        <button className="btn btn-lg btn-warning"
+                        onClick={cancelUpdate}>
+                            Cancel
+                        </button>
+                    </div>
+                </div>
+            </>
+    );
+};
+
+export default UpdateTask;
